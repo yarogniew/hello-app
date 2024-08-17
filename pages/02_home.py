5578c7655b8392370aa0145292be894eb186bae9
yarogniew <yarogniew@dpoczta.pl>
2024-08-17 18:59:05 +0200

    Update 02_home.py

--- pages/02_home.py
+++ pages/02_home.py
@@ -6,4 +6,4 @@ st.title("To jest strona Iwony Milewskiej")
 col1, col2 = st.columns([1, 1])
 
 uploades_photo = col2.file_uploader("Upload a photo")
-# col1.image("media/Iwonka_plaza_siedzaca_Emilia_1115 × 1672.jpg")
+col1.image("Iwonka_plaza_siedzaca_Emilia_1115 × 1672.jpg")
